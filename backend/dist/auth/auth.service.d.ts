e7a5840e46f6e5e938e5c45a4ee490a02b707d9d
Mihir Sen <67091503+mihirsen@users.noreply.github.com>
2025-03-14 23:51:19 +0530

    Initial commit: Smart City project with updated .gitignore

--- backend/dist/auth/auth.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { JwtService } from "@nestjs/jwt";
-export declare class AuthService {
-    private jwtService;
-    constructor(jwtService: JwtService);
-    generateToken(user: any): Promise<{
-        access_token: string;
-    }>;
-    validateToken(token: string): Promise<any>;
-}
